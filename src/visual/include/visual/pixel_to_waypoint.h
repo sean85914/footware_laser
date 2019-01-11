1777ca772b7c0fcef2bda718b03fdff28cc844b8
sean85914 <sean19960914@gmail.com>
2019-01-11 17:26:22 +0800

    go to nearest waypoint first

--- src/visual/include/visual/pixel_to_waypoint.h
+++ src/visual/include/visual/pixel_to_waypoint.h
@@ -1,6 +1,8 @@
 #ifndef PIXEL_TO_WAYPOINT_H
 #define PIXEL_TO_WAYPOINT_H
 
+#include <algorithm> // std::rotate
+
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
 // ROS message types
@@ -11,17 +13,20 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+// Eigen
+#include <Eigen/Dense>
 
 typedef pcl::PointCloud<pcl::PointXYZRGB> PointXYZRGB;
 
 const int EPSILON = 7; // 1-norm distance on neighborhood
 // Topic string
 const std::string PC_STR = "/camera/depth_registered/points"; // Pointcloud topic string
-const std::string PUB_STR = "/waypoint_array";
-const std::string SUB_STR = "/pixel_array";
+const std::string PUB_STR = "/waypoint_array"; // Publish topic string
+const std::string SUB_STR = "/pixel_array"; // Subscribe topic string
 // Frame string 
 const std::string CAMERA_STR = "camera_rgb_optical_frame"; // Pointcloud frame id
 const std::string BASE_STR = "base_link"; // Robot arm base link frame id
+const std::string TCP_STR = "tcp_link"; // TCP link frame id
 
 class Pixel2Waypoint
 {
@@ -29,16 +34,19 @@ class Pixel2Waypoint
   ros::NodeHandle nh_;
   ros::Subscriber sub;
   ros::Publisher pub;
-  geometry_msgs::PoseArray arr;
+  geometry_msgs::PoseArray arr; // output to publish
   PointXYZRGB pc_;
   bool valid = true, // Is the transformation from 2D to 3D is valid?
-       has_tf; // Is the transform from [camera_rgb_optical_frame] -> [base_link] exist?
+       has_tf; // Is the transform from [base_link] -> [camera_rgb_optical_frame] exist?
   tf::TransformListener listener;
   tf::StampedTransform transform;
-  tf::Matrix3x3 rot_mat; // Rotation matrix from [camera_rgb_optical_frame] -> [base_link]
-  tf::Vector3 trans; // Translation vector from [camera_rgb_optical_frame] -> [base_link]
+  tf::Matrix3x3 rot_mat; // Rotation matrix from [base_link] -> [camera_rgb_optical_frame]
+  tf::Vector3 trans, // Translation vector from [base_link] -> [camera_rgb_optical_frame]
+              x_vec, // TCP x-axis
+              tcp_point; // TCP point w.r.t base_link
   void getTransform(void); // Get transform, called when initialize
   void cb(const geometry_msgs::PoseArray msg); // Callback of subscriber
+  void reorder_array(void); // Re-order output pose array so that the first point is the nearest one
  public:
   Pixel2Waypoint(); // Constructor
 };
--- src/visual/src/pixel_to_waypoint.cpp
+++ src/visual/src/pixel_to_waypoint.cpp
@@ -30,7 +30,7 @@ void Pixel2Waypoint::cb(const geometry_msgs::PoseArray msg)
   pcl::fromROSMsg(*pc, pc_);
   for(int idx=0; idx<msg.poses.size(); ++idx){
     int pixel_x = msg.poses[idx].position.x,
-           pixel_y = msg.poses[idx].position.y;
+        pixel_y = msg.poses[idx].position.y;
     double wp_x = pc_.at(pixel_x, pixel_y).x,
            wp_y = pc_.at(pixel_x, pixel_y).y,
            wp_z = pc_.at(pixel_x, pixel_y).z;
@@ -67,6 +67,54 @@ void Pixel2Waypoint::cb(const geometry_msgs::PoseArray msg)
     } // end if(valid and has_tf)
   } // end for (idx)
   ROS_INFO("After processing, there are %d waypoints.", (int)arr.poses.size());
+  reorder_array();
   pub.publish(arr);
   ros::shutdown(); // Only process once;
+}
+
+void Pixel2Waypoint::reorder_array(void)
+{
+  // Least-square plane fitting
+  Eigen::MatrixXd A((int)arr.poses.size(), 3),
+                  x(3, 1),
+                  b((int)arr.poses.size(), 1);
+  // Build matrix A and b
+  for(int i=0; i<arr.poses.size(); ++i){
+    A(i, 0) = arr.poses[i].position.x;
+    A(i, 1) = arr.poses[i].position.y;
+    A(i, 2) = -1;
+    b(i, 0) = -arr.poses[i].position.z;
+  } // end for (i)
+  // x = (A'A)^-1*b
+  x = (A.transpose()*A).inverse()*b;
+  // Get the transform from [base_link] to [tcp_link]
+  try{
+    listener.waitForTransform(BASE_STR, TCP_STR, ros::Time(0), ros::Duration(3.0));
+    listener.lookupTransform(BASE_STR, TCP_STR, ros::Time(0), transform);
+    x_vec = transform.getBasis().getColumn(0);
+    tcp_point = transform.getOrigin();
+  } catch(tf::TransformException ex) {ROS_ERROR("%s", ex.what());}
+  // Get the projection point on the plane determined above
+  double p_a = x(0, 0), p_b = x(0, 1), p_c = x(0, 2), // Plane parameters
+         x_0 = tcp_point.getX(), y_0 = tcp_point.getY(), z_0 = tcp_point.getZ(), // TCP point
+         x_x = x_vec.getX(), x_y = x_vec.getY(), x_z = x_vec.getZ(), // x-axis vector
+         t_bar = (p_c-p_a*x_0-p_b*y_0-z_0)/(p_a*x_x+p_b*x_y+x_z); 
+         // Parameter value for line equation
+  double x_p = x_0 + x_x * t_bar, 
+         y_p = y_0 + x_y * t_bar, 
+         z_p = z_0 + x_z * t_bar; // Projection point on plane
+  double min_dis = 1e6;
+  int min_idx = -1;
+  // Calculate distance between each point to projection point and get the minimum index
+  for(int i=0; i<arr.poses.size(); ++i) {
+    double dx = x_p - arr.poses[i].position.x,
+           dy = y_p - arr.poses[i].position.y, 
+           dz = z_p - arr.poses[i].position.z,
+           dis_2 = dx*dx + dy*dy + dz*dz; // distance square
+    if(dis_2 < min_dis){
+      min_dis = dis_2; min_idx = i;
+    } // end if(dis_2 < min_dis)
+  } // end for (i)
+  // Circular shift toward right with min_idx
+  std::rotate(arr.poses.begin(), arr.poses.begin()+((int)arr.poses.size()-min_idx), arr.poses.end());
 }
